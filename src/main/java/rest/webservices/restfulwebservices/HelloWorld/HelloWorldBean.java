47eb520c32fed332bd8f5b74f38d259c7782bbb8
Avinash Kumar <akgpcamp@gmail.com>
2023-08-07 00:35:57 +0530

    First Push

---
+++ src/main/java/rest/webservices/restfulwebservices/HelloWorld/HelloWorldBean.java
@@ -0,0 +1,18 @@
+package rest.webservices.restfulwebservices.HelloWorld;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
+
+@Getter
+@Setter
+@ToString
+@AllArgsConstructor
+public class HelloWorldBean {
+    private String message;
+//
+//    public HelloWorldBean(String message) {
+//        this.message = message;
+//    }
+}
