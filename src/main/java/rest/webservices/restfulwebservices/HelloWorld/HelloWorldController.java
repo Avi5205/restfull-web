2e48b654a6844f294bc6661631d89c50f821cc96
Avinash Kumar <akgpcamp@gmail.com>
2023-08-07 00:41:34 +0530

    First Push

--- src/main/java/rest/webservices/restfulwebservices/HelloWorld/HelloWorldController.java
+++ src/main/java/rest/webservices/restfulwebservices/HelloWorld/HelloWorldController.java
@@ -2,7 +2,6 @@ package rest.webservices.restfulwebservices.HelloWorld;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -10,18 +9,18 @@ import org.springframework.web.bind.annotation.RestController;
 public class HelloWorldController {
 
     @GetMapping("/hello-world")
-    public String helloWorld(){
+    public String helloWorld() {
         return "HelloWorld";
     }
 
     @GetMapping("hello-world-bean")
-    public HelloWorldBean helloWorldBean(){
+    public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World");
     }
 
     @GetMapping("hello-world/path-variable/{name}")
-    public HelloWorldBean helloWorldPathVariable(@PathVariable String name){
-        return new HelloWorldBean(String.format("Hello %s" ,name));
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello %s", name));
     }
 
 }
