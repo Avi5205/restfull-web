7512eaecf73d2478d8b49a91364ad8ad732e7136
Avinash Kumar <akgpcamp@gmail.com>
2023-08-13 01:33:30 +0530

    Spring Security

---
+++ src/main/java/rest/webservices/restfulwebservices/security/SpringSecurityConfiguration.java
@@ -0,0 +1,17 @@
+package rest.webservices.restfulwebservices.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+public class SpringSecurityConfiguration {
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeHttpRequests(auth -> auth.anyRequest().authenticated());
+        http.httpBasic(withDefaults());
+        return http.build();
+    }
+}
